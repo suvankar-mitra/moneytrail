6a53cd84efd1c1ee6606eaeb9a1a0e59a6fb6d45
Suvankar Mitra <16486734+suvankar-mitra@users.noreply.github.com>
2026-03-19 13:15:37 +0530

    feat(contact): add Contact JPA repository

---
+++ src/main/java/cc/suvankar/moneytrail/contact/ContactRepository.java
@@ -0,0 +1,12 @@
+package cc.suvankar.moneytrail.contact;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepository extends JpaRepository<Contact, UUID> {
+
+    List<Contact> findByUserId(UUID userId);
+
+}
