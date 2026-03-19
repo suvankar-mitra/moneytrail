e69f55be28b16c863431179cffdf6e54fec8625f
Suvankar Mitra <16486734+suvankar-mitra@users.noreply.github.com>
2026-03-19 16:23:52 +0530

    fix(jpa): configure OffsetDateTime provider for Spring auditing

--- src/main/java/cc/suvankar/moneytrail/MoneytrailApplication.java
+++ src/main/java/cc/suvankar/moneytrail/MoneytrailApplication.java
@@ -1,15 +1,25 @@
 package cc.suvankar.moneytrail;
 
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditing(dateTimeProviderRef = "dateTimeProvider")
 public class MoneytrailApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoneytrailApplication.class, args);
+	}
+
+	@Bean
+	public DateTimeProvider dateTimeProvider() {
+		return () -> Optional.of(OffsetDateTime.now());
 	}
 
 }
