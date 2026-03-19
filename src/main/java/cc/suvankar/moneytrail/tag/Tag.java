8b330087deb437e2c5fdb8e1e5a7a119b28ec536
Suvankar Mitra <16486734+suvankar-mitra@users.noreply.github.com>
2026-03-19 12:28:43 +0530

    refactor(tag): remove unnecessary EntityListeners annotation

--- src/main/java/cc/suvankar/moneytrail/tag/Tag.java
+++ src/main/java/cc/suvankar/moneytrail/tag/Tag.java
@@ -1,11 +1,8 @@
 package cc.suvankar.moneytrail.tag;
 
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import cc.suvankar.moneytrail.user.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,7 +21,6 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 @Table(name = "tags")
-@EntityListeners(AuditingEntityListener.class)
 public class Tag {
     @Id
     @Column(name = "id", nullable = false, updatable = false)
