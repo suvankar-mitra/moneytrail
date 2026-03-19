8ad5fc1ae9e8d534c72635420bacc0bb05a61afa
Suvankar Mitra <16486734+suvankar-mitra@users.noreply.github.com>
2026-03-19 11:41:25 +0530

    feat(tag): add Tag JPA entity

---
+++ src/main/java/cc/suvankar/moneytrail/tag/Tag.java
@@ -0,0 +1,57 @@
+package cc.suvankar.moneytrail.tag;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import cc.suvankar.moneytrail.user.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tags")
+@EntityListeners(AuditingEntityListener.class)
+public class Tag {
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "tag_name", nullable = false)
+    private String tagName;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (id == null) {
+            return false;
+        }
+        if (obj instanceof Tag other) {
+            return other.id.equals(id);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Long.hashCode(id);
+    }
+
+}
