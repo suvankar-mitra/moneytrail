325ec80b1830f47d4484f74058891139c7355268
Suvankar Mitra <16486734+suvankar-mitra@users.noreply.github.com>
2026-03-19 13:02:33 +0530

    feat(user): add User JPA repository

---
+++ src/main/java/cc/suvankar/moneytrail/user/UserRepository.java
@@ -0,0 +1,12 @@
+package cc.suvankar.moneytrail.user;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+
+}
