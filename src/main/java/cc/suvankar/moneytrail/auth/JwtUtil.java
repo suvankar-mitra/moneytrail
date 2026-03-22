7eb915677687f440bb171b7ceb2959c03c0d91eb
Suvankar Mitra <16486734+suvankar-mitra@users.noreply.github.com>
2026-03-22 23:08:55 +0530

    feat(auth): add JwtUtil for token generation and validation

---
+++ src/main/java/cc/suvankar/moneytrail/auth/JwtUtil.java
@@ -0,0 +1,56 @@
+package cc.suvankar.moneytrail.auth;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${app.jwt.secret}")
+    private String secret;
+
+    @Value("${app.jwt.expiration}")
+    private Long expiration;
+
+    private SecretKey getSignedKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSignedKey())
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parser()
+                .verifyWith(getSignedKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(getSignedKey())
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch(JwtException e) {
+            return false;
+        }
+    }
+}
