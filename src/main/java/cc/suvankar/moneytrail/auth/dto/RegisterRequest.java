807717866cac447ba482dfd5528277f96ff8a953
Suvankar Mitra <16486734+suvankar-mitra@users.noreply.github.com>
2026-03-23 20:46:56 +0530

    feat(auth): add Register, Login and Authentication DTOs

---
+++ src/main/java/cc/suvankar/moneytrail/auth/dto/RegisterRequest.java
@@ -0,0 +1,26 @@
+package cc.suvankar.moneytrail.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Size(min = 8)
+    private String password;
+}
