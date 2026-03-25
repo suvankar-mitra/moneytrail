b105954328e604b9ce5f7911280795e6d3e3f91d
Suvankar Mitra <16486734+suvankar-mitra@users.noreply.github.com>
2026-03-25 18:38:58 +0530

    refactor(config): move JPA config from MoneytrailApplication to its own class

--- src/main/java/cc/suvankar/moneytrail/MoneytrailApplication.java
+++ src/main/java/cc/suvankar/moneytrail/MoneytrailApplication.java
@@ -1,25 +1,12 @@
 package cc.suvankar.moneytrail;
 
-import java.time.OffsetDateTime;
-import java.util.Optional;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.auditing.DateTimeProvider;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing(dateTimeProviderRef = "dateTimeProvider")
 public class MoneytrailApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoneytrailApplication.class, args);
-	}
-
-	@Bean
-	public DateTimeProvider dateTimeProvider() {
-		return () -> Optional.of(OffsetDateTime.now());
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MoneytrailApplication.class, args);
+    }
 }
