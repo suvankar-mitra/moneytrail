19d097e11fa2af8f4223a9792300e0dcdec83253
Suvankar Mitra <16486734+suvankar-mitra@users.noreply.github.com>
2026-04-01 23:16:06 +0530

    test(auth): add integration tests for login and register endpoints

--- src/main/java/cc/suvankar/moneytrail/config/SecurityConfig.java
+++ src/main/java/cc/suvankar/moneytrail/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated()
                 )
---
+++ src/test/java/cc/suvankar/moneytrail/auth/AuthControllerIT.java
@@ -0,0 +1,144 @@
+package cc.suvankar.moneytrail.auth;
+
+import cc.suvankar.moneytrail.user.User;
+import cc.suvankar.moneytrail.user.UserRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test-h2")
+@Transactional
+public class AuthControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private final static String registerBaseUrl = "/api/v1/auth/register";
+    private final static String loginBaseUrl = "/api/v1/auth/login";
+
+    @Test
+    public void register_shouldReturn201_whenValidRequest() throws Exception {
+        String validJsonRequest = """
+                {
+                    "name": "John Doe",
+                    "email": "john@example.com",
+                    "password": "password123"
+                }
+                """;
+
+        mockMvc.perform(post(registerBaseUrl).contentType(MediaType.APPLICATION_JSON).content(validJsonRequest))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void register_shouldReturn400_whenEmailIsMalformed() throws Exception {
+        String invalidJsonRequest = """
+                {
+                    "name": "John Doe",
+                    "email": "john#exampe_com",
+                    "password": "password123"
+                }
+                """;
+
+        mockMvc.perform(post(registerBaseUrl).contentType(MediaType.APPLICATION_JSON).content(invalidJsonRequest))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").isString())
+                .andExpect(jsonPath("$.message").value("email: Must be a valid email address"));
+    }
+
+    @Test
+    public void register_shouldReturn409_whenEmailAlreadyExists() throws Exception {
+        var user = new User();
+        user.setEmail("john@example.com");
+        user.setName("Johny Doe");
+        user.setPasswordHash(passwordEncoder.encode("password123"));
+        userRepository.save(user);
+
+        String dupEmailJsonRequest = """
+                {
+                    "name": "John Doe",
+                    "email": "john@example.com",
+                    "password": "password123"
+                }
+                """;
+
+        mockMvc.perform(post(registerBaseUrl).contentType(MediaType.APPLICATION_JSON).content(dupEmailJsonRequest))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.message").exists());
+    }
+
+
+    @Test
+    public void login_shouldReturn200AndJwtToken_whenValidCredential() throws Exception {
+        var user = new User();
+        user.setEmail("john@example.com");
+        user.setName("John Doe");
+        user.setPasswordHash(passwordEncoder.encode("password123"));
+        userRepository.save(user);
+
+        String validJsonRequest = """
+                {
+                    "email": "john@example.com",
+                    "password": "password123"
+                }
+                """;
+
+        mockMvc.perform(post(loginBaseUrl).contentType(MediaType.APPLICATION_JSON).content(validJsonRequest))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.token").exists());
+    }
+
+    @Test
+    public void login_shouldReturn401_whenIncorrectCredential() throws Exception {
+        var user = new User();
+        user.setEmail("john@example.com");
+        user.setName("John Doe");
+        user.setPasswordHash(passwordEncoder.encode("password123"));
+        userRepository.save(user);
+
+        String validJsonRequest = """
+                {
+                    "email": "john@example.com",
+                    "password": "password12"
+                }
+                """;
+
+        mockMvc.perform(post(loginBaseUrl).contentType(MediaType.APPLICATION_JSON).content(validJsonRequest))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.token").doesNotExist());
+    }
+
+    @Test
+    public void login_shouldReturn400_whenInvalidEmail() throws Exception {
+        String validJsonRequest = """
+                {
+                    "email": "john#example.com",
+                    "password": "password123"
+                }
+                """;
+
+        mockMvc.perform(post(loginBaseUrl).contentType(MediaType.APPLICATION_JSON).content(validJsonRequest))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.token").doesNotExist())
+                .andExpect(jsonPath("$.message").exists());
+    }
+
+}
