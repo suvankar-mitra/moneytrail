28dc2bb7332772dd7f2e343260f02f3120297fd1
Suvankar Mitra <16486734+suvankar-mitra@users.noreply.github.com>
2026-04-18 23:19:35 +0530

    feat(contact): add repository method to fetch by user and contact id

--- src/main/java/cc/suvankar/moneytrail/contact/ContactRepository.java
+++ src/main/java/cc/suvankar/moneytrail/contact/ContactRepository.java
@@ -1,12 +1,14 @@
 package cc.suvankar.moneytrail.contact;
 
-import java.util.List;
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface ContactRepository extends JpaRepository<Contact, UUID> {
 
     List<Contact> findByUserId(UUID userId);
 
+    Optional<Contact> findByUserIdAndId(UUID userId, UUID id);
 }
