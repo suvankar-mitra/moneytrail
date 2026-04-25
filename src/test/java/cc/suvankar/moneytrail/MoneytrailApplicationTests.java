95f64c05922f8de75a784278785c546efc586ad0
Suvankar Mitra <16486734+suvankar-mitra@users.noreply.github.com>
2026-04-25 21:19:48 +0530

    chore(test): link h2 profile to springboot tests

--- src/test/java/cc/suvankar/moneytrail/MoneytrailApplicationTests.java
+++ src/test/java/cc/suvankar/moneytrail/MoneytrailApplicationTests.java
@@ -2,8 +2,10 @@ package cc.suvankar.moneytrail;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test-h2")
 class MoneytrailApplicationTests {
 
   @Test
