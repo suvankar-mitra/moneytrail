20c4f76c88fa4c21bec288dd08ec01d7d37f8a01
Suvankar Mitra <16486734+suvankar-mitra@users.noreply.github.com>
2026-05-30 11:51:03 +0530

    feat(transactions): add exchange ratio column

--- src/main/resources/db/migration/V4__create_transactions.sql
+++ src/main/resources/db/migration/V4__create_transactions.sql
@@ -1,8 +1,9 @@
 CREATE TABLE transactions(
     id UUID PRIMARY KEY,
-    from_account_id UUID NOT NULL REFERENCES accounts(id) ON DELETE RESTRICT,
-    to_account_id UUID NOT NULL REFERENCES accounts(id) ON DELETE RESTRICT,
-    amount DECIMAL(19, 4) NOT NULL CHECK(amount > 0),
+    from_account_id UUID NOT NULL CONSTRAINT fk_transactions_from_account REFERENCES accounts(id) ON DELETE RESTRICT,
+    to_account_id UUID NOT NULL CONSTRAINT fk_transactions_to_account REFERENCES accounts(id) ON DELETE RESTRICT,
+    transaction_amount DECIMAL(19, 4) NOT NULL CONSTRAINT ck_transactions_amount CHECK(transaction_amount > 0),
+    exchange_ratio DECIMAL(18, 6) NOT NULL DEFAULT 1.0 CONSTRAINT ck_transactions_xch_ratio CHECK(exchange_ratio > 0),
     tran_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
     note TEXT,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
